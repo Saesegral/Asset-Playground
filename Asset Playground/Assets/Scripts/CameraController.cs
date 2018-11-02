fc26195fb6887f46f0ed3f30bd16e0cc01d8a25c
Saesegral <stevenkgrigsby@gmail.com>
2018-11-01 22:48:37 -0500

    Setup Basics
    Put in all the basic scripts for testing out movement and camera control, placed a few models in, configured the goblin avatar.

---
+++ Asset Playground/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject followTarget;
+
+    private Vector3 displacement;
+
+    [SerializeField] private int closestZoom = 1;
+    [SerializeField] private int farthestZoom = 10;
+    private Camera cam;
+    
+	void Start () {
+        cam = GetComponent<Camera>();
+        displacement = cam.transform.position - followTarget.transform.position;
+	}
+	
+	void Update () {
+        cam.transform.position = followTarget.transform.position + displacement;
+        if (cam.orthographic) {
+            if (Input.GetAxis("Mouse ScrollWheel") > 0f && cam.orthographicSize > closestZoom) {
+                cam.orthographicSize--;
+            } else if (Input.GetAxis("Mouse ScrollWheel") < 0f && cam.orthographicSize < farthestZoom) {
+                cam.orthographicSize++;
+            }
+        }
+    }
+}
